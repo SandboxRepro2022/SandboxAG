35350657e54cc65378166de9e3bc59f74b849740
Auguste <auguste.gardette@agroparistech.fr>
2022-09-16 15:08:45 +0000

    changed py

--- plotdata.py
+++ plotdata.py
@@ -1,7 +1,8 @@
 #! /bin/python3
 
+import pandas as pd
 
-
-coviddata = read.table("covid_aggregated.txt")
-plot(coviddata$V1,coviddata$V2)
-lines(coviddata$V1,coviddata$V2)
+coviddata = pd.read_table("covid_aggregated.txt")
+print(coviddata.head())
+#plot(coviddata[1],coviddata[2])
+#lines(coviddata[1],coviddata[2])
